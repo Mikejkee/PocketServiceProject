c62623f5fccd5f7f382a18ce8176d4f2a6b6eded
Mikejkee <mikemosin@icloud.com>
2023-06-13 19:30:25 +0300

    Update prod settings, add compose, prod dockerfiles, connect and add nginx

--- hcs_hr/settings.py
+++ hcs_hr/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     # os.path.join(os.path.dirname(os.path.abspath(__file__)), "static"),
     os.path.join(BASE_DIR, "static"),
