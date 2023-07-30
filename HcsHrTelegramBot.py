6856e9fecdd932c13b7945d3fecd95ecab907753
git config --global user.email Your <yumimo@yandex.ru>
2023-07-30 17:33:49 +0300

    fix problem with NGINX, repair webApp

--- HcsHrTelegramBot.py
+++ HcsHrTelegramBot.py
@@ -148,19 +148,19 @@ async def showcase(message: Message):
 
     if not webapp_data:
         webApp_flat_repair = WebAppInfo(
-            url=f'{CREW_URL}/PocketServiceApp/showcase/?TelegramId={client_id}&ShowcaseType=0')
+            url=f'https://{CREW_URL}/PocketServiceApp/showcase/?TelegramId={client_id}&ShowcaseType=0')
         button_0 = KeyboardButton(text='Ремонт квартиры', web_app=webApp_flat_repair)
 
         webApp_technique_repair = WebAppInfo(
-            url=f'{CREW_URL}/PocketServiceApp/showcase/?TelegramId={client_id}&ShowcaseType=1')
+            url=f'https://{CREW_URL}/PocketServiceApp/showcase/?TelegramId={client_id}&ShowcaseType=1')
         button_1 = KeyboardButton(text='Ремонт техники', web_app=webApp_technique_repair)
 
         webApp_furniture_repair = WebAppInfo(
-            url=f'{CREW_URL}/PocketServiceApp/showcase/?TelegramId={client_id}&ShowcaseType=2')
+            url=f'https://{CREW_URL}/PocketServiceApp/showcase/?TelegramId={client_id}&ShowcaseType=2')
         button_2 = KeyboardButton(text='Ремонт мебели', web_app=webApp_furniture_repair)
 
         webApp_beauty_services = WebAppInfo(
-            url=f'{CREW_URL}/PocketServiceApp/showcase/?TelegramId={client_id}&ShowcaseType=2')
+            url=f'https://{CREW_URL}/PocketServiceApp/showcase/?TelegramId={client_id}&ShowcaseType=2')
         button_3 = KeyboardButton(text='Услуги красоты', web_app=webApp_beauty_services)
 
         button_back = KeyboardButton(text='🔙 Главное меню')
--- PocketServiceApp/consumers.py
+++ PocketServiceApp/consumers.py
@@ -10,14 +10,23 @@ class ClientStatusConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
         await self.accept()
+        await self.send(text_data=json.dumps({'status': 'connected'}))
+        await self.close()
+    async def receive(self, event):
+        data = event['data']
+        print(f'BOT EVENT {event}')
+        await self.send(text_data=json.dumps(data))
+        await self.close()
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             'client',
             self.channel_name
         )
+        await self.close()
 
-    async def update_task_status(self, event):
-        data = event['data']
-        print(f'BOT EVENT {event}')
-        await self.send(text_data=json.dumps(data))
+    # async def update_task_status(self, event):
+    #     data = event['data']
+    #     print(f'BOT EVENT {event}')
+    #     await self.send(text_data=json.dumps(data))
+    #     await self.close()
